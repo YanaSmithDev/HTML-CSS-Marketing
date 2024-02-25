493f722ff998b2795bcb7ccc8dbf562da9a52938
Yana Kuznetsova <yana.kuznetsova.dev@gmail.com>
2024-02-25 19:29:49 +0300

    feat: add adaptive

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 $(function () {
-  var mixer = mixitup('.portfolio__content');
-
+  
   $('.slider-blog__inner').slick({
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow left"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow right"></button>',
   });
+
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active');
+    $('.top-inner').toggleClass('top-inner--active');
+  });
+
+
+
+  var mixer = mixitup('.portfolio__content');
+
 });
 
